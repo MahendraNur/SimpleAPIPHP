5e5383df92424dc7b9eca21853d46a465a1c409e
MahendraNur <mahendranurpramudiansyah@gmail.com>
2023-09-07 22:45:07 +0700

    first commit

---
+++ db_kampus.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 07 Sep 2023 pada 17.25
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_kampus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_mahasiswa`
+--
+
+CREATE TABLE `tbl_mahasiswa` (
+  `id` int(11) NOT NULL,
+  `nim` char(10) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `jk` char(1) NOT NULL,
+  `alamat` text NOT NULL,
+  `jurusan` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tbl_mahasiswa`
+--
+
+INSERT INTO `tbl_mahasiswa` (`id`, `nim`, `nama`, `jk`, `alamat`, `jurusan`) VALUES
+(1, '223443083', 'Mahendra Nur Pramudiansyah', 'L', 'JL LW GAJAH GG MELATI/13', 'AUTOMATION ENGINEERING');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tbl_mahasiswa`
+--
+ALTER TABLE `tbl_mahasiswa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_mahasiswa`
+--
+ALTER TABLE `tbl_mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "db_kampus";
+
+$mysqli = new mysqli($host, $user, $pass, $db);
+// if ($mysqli) {
+//     echo("Connection successful");
+// }else{
+//     echo("Connection failed");
+// }
+?>
